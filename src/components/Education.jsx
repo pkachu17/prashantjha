d679682784538a9c379de6963442ace1e0a0e37b
Prashant Jha <user.name>
2022-11-29 23:36:40 -0500

    commit

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,80 +1,106 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { Chrono } from 'react-chrono';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 import { Container } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
-import Fade from 'react-reveal';
 import { ThemeContext } from 'styled-components';
-import endpoints from '../constants/endpoints';
+import Fade from 'react-reveal';
 import Header from './Header';
+import endpoints from '../constants/endpoints';
 import FallbackSpinner from './FallbackSpinner';
 import '../css/education.css';
+
+const styles = {
+  ulStyle: {
+    listStylePosition: 'outside',
+    paddingLeft: 50,
+  },
+  subtitleContainerStyle: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  subtitleStyle: {
+    display: 'inline-block',
+  },
+  inlineChild: {
+    display: 'inline-block',
+  },
+  itemStyle: {
+    marginBottom: 10,
+  },
+};
 
 function Education(props) {
   const theme = useContext(ThemeContext);
   const { header } = props;
   const [data, setData] = useState(null);
-  const [width, setWidth] = useState('50vw');
-  const [mode, setMode] = useState('VERTICAL_ALTERNATING');
 
   useEffect(() => {
     fetch(endpoints.education, {
       method: 'GET',
     })
       .then((res) => res.json())
-      .then((res) => setData(res))
+      .then((res) => setData(res.educations))
       .catch((err) => err);
-
-    if (window?.innerWidth < 576) {
-      setMode('VERTICAL');
-    }
-
-    if (window?.innerWidth < 576) {
-      setWidth('90vw');
-    } else if (window?.innerWidth >= 576 && window?.innerWidth < 768) {
-      setWidth('90vw');
-    } else if (window?.innerWidth >= 768 && window?.innerWidth < 1024) {
-      setWidth('75vw');
-    } else {
-      setWidth('50vw');
-    }
   }, []);
 
   return (
     <>
       <Header title={header} />
-      {data ? (
-        <Fade>
-          <div style={{ width }} className="section-content-container">
+
+      {data
+        ? (
+          <div className="section-content-container">
             <Container>
-              <Chrono
-                hideControls
-                allowDynamicUpdate
-                useReadMore={false}
-                items={data.education}
-                cardHeight={250}
-                mode={mode}
-                theme={{
-                  primary: theme.accentColor,
-                  secondary: theme.accentColor,
-                  cardBgColor: theme.chronoTheme.cardBgColor,
-                  cardForeColor: theme.chronoTheme.cardForeColor,
-                  titleColor: theme.chronoTheme.titleColor,
-                }}
+              <Timeline
+                lineColor={theme.timelineLineColor}
               >
-                <div className="chrono-icons">
-                  {data.education.map((education) => (education.icon ? (
-                    <img
-                      key={education.icon.src}
-                      src={education.icon.src}
-                      alt={education.icon.alt}
-                    />
-                  ) : null))}
-                </div>
-              </Chrono>
+                {data.map((item) => (
+                  <Fade>
+                    <TimelineItem
+                      key={item.title + item.dateText}
+                      dateText={item.dateText}
+                      dateInnerStyle={{ background: theme.accentColor }}
+                      style={styles.itemStyle}
+                      bodyContainerStyle={{ color: theme.color }}
+                    >
+                      <h2 className="item-title">
+                        {item.title}
+                      </h2>
+                      <div style={styles.subtitleContainerStyle}>
+                        <h4 style={{ ...styles.subtitleStyle, color: theme.accentColor }}>
+                          {item.subtitle}
+                        </h4>
+                        {item.courseType && (
+                        <h5 style={styles.inlineChild}>
+                        &#127891;
+                          {' '}
+                          {item.courseType}
+                        </h5>
+                        )}
+                      </div>
+                      <ul style={styles.ulStyle}>
+                        {item.courseDescription.map((point) => (
+                          <div key={point}>
+                            <li>
+                              <ReactMarkdown
+                                children={point}
+                                components={{
+                                  p: 'span',
+                                }}
+                              />
+                            </li>
+                            <br />
+                          </div>
+                        ))}
+                      </ul>
+                    </TimelineItem>
+                  </Fade>
+                ))}
+              </Timeline>
             </Container>
           </div>
-        </Fade>
-      ) : <FallbackSpinner /> }
+        ) : <FallbackSpinner /> }
     </>
   );
 }
